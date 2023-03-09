47fc4da6e6d9a603ad0849b2a1ae3f3d5a6c2107
mdallow <mikedallow@gmail.com>
2023-03-08 22:20:50 -0800

    Add dir_size and bytes_to_str functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,56 @@
-use std::{path::{Path, PathBuf}, fs};
 use filesize::PathExt;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
+// TODO: this function almost exactly mimics list_files,
+//       there is some optimazation to be found
+fn dir_size(path: &Path) -> u64 {
+    let mut dirs: Vec<PathBuf> = Vec::new();
+    let mut size: u64 = 0;
 
+    let cur_dir = match fs::read_dir(path) {
+        Ok(dir) => dir,
+        Err(_) => {
+            println!("{}: Cannot open dir!", path.display());
+            return 0;
+        }
+    };
+
+    for entry in cur_dir {
+        let entry_path = entry.unwrap().path();
+
+        let metadata = match fs::metadata(&entry_path) {
+            Ok(data) => data,
+            Err(_) => {
+                println!("{}: Denied access to metadata!", entry_path.display());
+                continue;
+            }
+        };
+
+        if metadata.is_file() {
+            let filesize = match entry_path.size_on_disk() {
+                Ok(size) => size,
+                Err(_) => {
+                    println!("{}: Cannot determine file size!", entry_path.display());
+                    continue;
+                }
+            };
+            size += filesize;
+        } else if metadata.is_dir() {
+            dirs.push(entry_path.clone());
+        } else {
+            println!("{}: Entry type unknown!", entry_path.display());
+        }
+    }
+
+    for dir in dirs {
+        size += dir_size(&dir);
+    }
+
+    return size;
+}
 
 fn list_files(path: &Path, depth: Option<u32>) -> Vec<(PathBuf, u64)> {
     let mut dirs: Vec<PathBuf> = Vec::new();
@@ -11,37 +60,33 @@ fn list_files(path: &Path, depth: Option<u32>) -> Vec<(PathBuf, u64)> {
         Ok(dir) => dir,
         Err(_) => {
             println!("{}: Cannot open dir!", path.display());
-            return vec![]
+            return vec![];
         }
     };
 
     for entry in cur_dir {
         let entry_path = entry.unwrap().path();
-        
+
         let metadata = match fs::metadata(&entry_path) {
             Ok(data) => data,
             Err(_) => {
                 println!("{}: Denied access to metadata!", entry_path.display());
-                continue
+                continue;
             }
         };
 
-        
         if metadata.is_file() {
             let filesize = match entry_path.size_on_disk() {
                 Ok(size) => size,
                 Err(_) => {
                     println!("{}: Cannot determine file size!", entry_path.display());
-                    continue
-                },
+                    continue;
+                }
             };
             file_and_sizes.push((entry_path, filesize));
-            // println!("{}, {}", entry_path.display(), filesize);
-        }
-        else if metadata.is_dir() {
+        } else if metadata.is_dir() {
             dirs.push(entry_path.clone());
-        }
-        else {
+        } else {
             println!("{}: Entry type unknown!", entry_path.display());
         }
     }
@@ -49,35 +94,72 @@ fn list_files(path: &Path, depth: Option<u32>) -> Vec<(PathBuf, u64)> {
     if let Some(n) = depth {
         if n > 0 {
             for dir in dirs {
-                file_and_sizes.append(&mut list_files(&dir, Some(n-1)));
+                file_and_sizes.append(&mut list_files(&dir, Some(n - 1)));
             }
         }
-    }
-    else {
+    } else {
         for dir in dirs {
             file_and_sizes.append(&mut list_files(&dir, None));
         }
     }
-    
+
     return file_and_sizes;
 }
 
-
 fn find_n_largest_files(mut files: Vec<(PathBuf, u64)>, n: usize) -> Vec<(PathBuf, u64)> {
     files.sort_by(|a, b| b.1.cmp(&a.1));
-    
+
     if files.len() <= n {
         return files.clone();
-    }
-    else {
+    } else {
         return files[..n].to_vec();
     }
 }
 
-fn main() {
-    let path = Path::new("C:\\Users\\");
+// TODO: write a unit test for this
+fn bytes_size_to_str(size: u64) -> String {
+    let size_map = Vec::from([
+        ((10 as u64).pow(0), "B"),
+        ((10 as u64).pow(3), "KB"),
+        ((10 as u64).pow(6), "MB"),
+        ((10 as u64).pow(9), "GB"),
+        ((10 as u64).pow(12), "TB"),
+        ((10 as u64).pow(15), "PB"),
+    ]);
 
-    let file_list = list_files(path, Some(4));
-    let largest = find_n_largest_files(file_list, 3);
+    let mut last_x = size as f64;
+    // FIXME: feels like there should be a more efficient way than to clone below
+    for (i, (exp, s)) in size_map.clone().into_iter().enumerate() {
+        let x = (size as f64) / (exp as f64);
+
+        if x <= 1.0 {
+            if i == 0 {
+                return format!("{:.2} {}", last_x, s.to_string());
+            } else {
+                return format!("{:.2} {}", last_x, size_map[i - 1].1.to_string());
+            }
+        }
+        last_x = x;
+    }
+
+    return format!(
+        "{:.2} {}",
+        last_x,
+        size_map[size_map.len() - 1].1.to_string()
+    );
+}
+
+fn main() {
+    let path = Path::new("C:\\");
+
+    let size = dir_size(path);
+    println!(
+        "Path {} has size {}",
+        path.display(),
+        bytes_size_to_str(size)
+    );
+
+    let file_list = list_files(path, None);
+    let largest = find_n_largest_files(file_list, 10);
     println!("Largest files: {:?}", largest);
 }
